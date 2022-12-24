359c46368fbfdea06913c4d34819a7bca89fe800
Yousuf Ahmad <yousuf@qualyval.com>
2022-12-24 16:50:27 +0530

    add filter on products

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -2,8 +2,35 @@ exports.Query = {
     hello: () => {
         return "World";
     },
-    products: (parent, args, {products}) => {
-        return products;
+    products: (parent, {filter}, {products, reviews}) => {
+        let filteredProducts = products;
+
+        if(filter) {
+            const { onSale, avgRating } = filter;
+            if(onSale) {
+                filteredProducts = filteredProducts.filter((product) => {
+                    return product.onSale === filter.onSale;
+                });
+            }
+
+            if([1,2,3,4,5].includes(avgRating)){
+                filteredProducts = filteredProducts.filter((product) => {
+                    let sumRating = 0;
+                    let numberOfReviews = 0
+                    reviews.forEach((review) => {
+                        if(review.productId === product.id) {
+                            sumRating += review.rating;
+                            numberOfReviews++;
+                        }
+                    });
+                    const avgProductRating = sumRating/numberOfReviews;
+                    // console.log(avgProductRating,product.name);
+                    return avgProductRating >= avgRating;
+                });
+            }
+        }
+
+        return filteredProducts;
     },
     product: (parent, args, {products}) => {
         let {id} = args;
