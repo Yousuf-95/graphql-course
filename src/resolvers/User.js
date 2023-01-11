509612aeb57dc8b7de6629fad4f456df891830ee
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-01-11 13:17:20 +0530

    bug fix

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -7,7 +7,21 @@ const User = {
                 return await posts.find({"authorId": id});
             }
             else{
-                return await posts.find({ "$and": [ {"authorId": id}, { "publishStatus": true } ]}).lean();
+                let post = await posts.find({ "$and": [ {"authorId": id}, { "publishStatus": true } ]}).lean();
+
+                let postsArray = [];
+                for(let i = 0; i < post.length; i++) {
+                    let currentPost = post[i];
+
+                    currentPost = {
+                        id: currentPost._id,
+                        ...currentPost
+                    }
+
+                    postsArray.push(currentPost);
+                }
+
+                return postsArray;
             }
         }
         catch(error){
